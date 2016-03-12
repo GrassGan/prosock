c7c89327ff9fdf23912b72b1f664dd71b4023448
ant <anth1y@gmail.com>
2016-03-12 05:56:50 -0500

    Adding protobufs

--- src/server.go
+++ src/server.go
@@ -1,7 +1,35 @@
 package main
 
 import (
-"os"
-"fmt"
 "net"
+"fmt"
+"os"
 )
+
+func handleConnect(c net.Conn){
+
+
+}
+
+func main() {
+   ln, err := net.ListenUnix("unix", &net.UnixAddr{"/tmp/funix", "unix"})
+   if err != nil {
+     panic(err)
+   }
+   defer os.Remove("/tmp/funix")
+
+   for {
+       conn, err := ln.AcceptUnix()
+       if err != nil {
+         panic(err)
+       }
+      var buf [1024]byte
+      n, err := conn.Read(buf[:])
+      if err != nil {
+        panic(err)
+      }
+      fmt.Printf("%s\n", string(buf[:n]));
+      conn.Close()
+   }
+
+}
